16799b04475196ab28188b8735b0beb04cc0efe7
Thiago Santos Oliveira <thiagosantos.on@gmail.com>
2020-05-17 23:53:11 -0300

    Create 1306_3.cpp
    Numerando Estradas

---
+++ 1306_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    double R, N;
+    cin >> R >> N;
+    double num;
+    int count = 1;
+    while(R!=0 && N!=0){
+        cout << "Case " << count << ": ";
+        if(N+N*26 > R){
+            num = (R - N) / N;
+            if(num<0){
+                num = 0;
+            }
+            cout << ceil(num) << endl; 
+        }else{
+            cout << "impossible" << endl;
+        }
+        count++;
+        cin >> R >> N;
+    }
+}
